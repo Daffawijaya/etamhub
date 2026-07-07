4202b1d0f1330fd50f4c9667e4985138a8a965ca
Daffa Yan Wijaya <daffayanwijaya@github.com>
2026-07-07 09:18:57 +0800

    mini update

--- src/app/umkm/[slug]/page.tsx
+++ src/app/umkm/[slug]/page.tsx
@@ -20,18 +20,15 @@ export default async function UmkmPage({ params }: Props) {
   }
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <div className="absolute inset-0 z-0 pointer-events-none overflow-hidden">
-        <div className="absolute top-0 left-0 w-[500px] h-[500px] rounded-full bg-violet-400/15 blur-3xl" />
-
-        <div className="absolute top-[400px] right-0 w-[600px] h-[600px] rounded-full bg-blue-400/15 blur-3xl" />
-
-        <div className="absolute bottom-0 left-1/3 w-[500px] h-[500px] rounded-full bg-pink-400/15 blur-3xl" />
-      </div>
-      <main className="w-full max-w-7xl mx-auto px-6 py-10 mt-20">
+      <div>
+        <main className="mt-20 relative z-10">
         <UmkmDetail {...umkm} />
       </main>
+      <Footer/>
+      </div>
+      
     </div>
   );
 }
