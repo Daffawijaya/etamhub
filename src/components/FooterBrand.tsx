cf661bbe6c454e4bde52ed8cd1fd6a688ccb265d
unknown <daffayanwijaya.com>
2026-07-16 15:44:34 +0800

    update display footerbrand

--- src/components/FooterBrand.tsx
+++ src/components/FooterBrand.tsx
@@ -1,11 +1,11 @@
 export default function FooterBrand() {
   return (
-    <section className="relative h-[250px] md:h-[250px] lg:h-[320px] overflow-hidden bg-dark">
+    <section className="relative h-[250px] md:h-[250px] lg:h-[300px] overflow-hidden bg-dark">
       {/* Fade atas */}
       <div className="absolute inset-x-0 top-0 h-10 bg-dark z-10" />
 
       {/* Tulisan besar */}
-      <div className="absolute inset-x-0 bottom-[-12%] flex justify-center">
+      <div className="absolute inset-x-0 bottom-[-15%] flex justify-center">
         <div
           className="
             px-10
