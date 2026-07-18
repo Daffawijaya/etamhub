9ad4933fc1bc3626b1954594766fef3933e6fdde
unknown <daffayanwijaya.com>
2026-07-18 17:42:55 +0800

    change incon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: "EtamHub",
   description: "Katalog UMKM Kutai Kartanegara",
   icons: {
-    icon: "/eicon4.png",
+    icon: "/eiconl.png",
   },
 };
 
