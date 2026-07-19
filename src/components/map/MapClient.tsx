359bee4d0a425fa1b73b6c21168711100e35c19f
unknown <daffayanwijaya.com>
2026-07-19 13:22:17 +0800

    last

--- src/components/map/MapClient.tsx
+++ src/components/map/MapClient.tsx
@@ -51,7 +51,7 @@ export default function MapClient() {
           url={
             resolvedTheme === "dark"
               ? "https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
-              : "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+              : "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           }
         />
 
